5f136ebc132fc32291daeb59c32b95c6ee29e0cc
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-10-24 10:41:32 +0200

    try formdata constructor

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-function displayData() {
-    const name = document.getElementById('name').value;
-    const surname = document.getElementById('surname').value;
-    const dob = document.getElementById('dob').value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const city = document.getElementById('city').value;
-    const address = document.getElementById('address').value;
-    const languages = document.querySelectorAll('input[name="language"]:checked');
-
-    let languagesList = '';
+/*     let languagesList = '';
     languages.forEach(lang => {
         languagesList += lang.value + ', ';
     });
     languagesList = languagesList.slice(0, -2);  // Видалення останньої коми
+ */
 
-    const tableHTML = `
-        <table class="table table-bordered">
-            <tr><td>Ім'я</td><td>${name}</td></tr>
-            <tr><td>Прізвище</td><td>${surname}</td></tr>
-            <tr><td>Дата народження</td><td>${dob}</td></tr>
-            <tr><td>Стать</td><td>${gender}</td></tr>
-            <tr><td>Місто</td><td>${city}</td></tr>
-            <tr><td>Адреса</td><td>${address}</td></tr>
-            <tr><td>Мови</td><td>${languagesList}</td></tr>
-        </table>    `;
 
-    document.getElementById('outputTable').innerHTML = tableHTML;
-    document.getElementById('registrationForm').style.display = 'none';
+const form = document.getElementById('registrationForm');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Використовуємо FormData для збору даних з форми
+    const formData = new FormData(event.target);
+
+    const userData = {};
+    for (let [key, value] of formData) {
+        userData[key] = value;
+    }
+    
+    // Виводимо дані в консоль
+    console.log(userData);
+
+    // І виводимо дані в таблицю
+    displayData(userData);
+});
+
+function displayData(data) {
+    const name = data.name;
+    const surname = data.surname;
+    const dob = data.dob;
+    const gender = data.gender;
+    const city = data.city;
+    const address = data.address;
+    const languages = data.language;  // тут можуть бути деякі відмінності, залежно від того, як ви зберігаєте мови
+
+    // Інший код вашої функції ...
 }
+
