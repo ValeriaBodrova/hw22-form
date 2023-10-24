b8087f03888074ef27014fc1b9b0c66dd729552b
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-10-24 10:56:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,18 @@ form.addEventListener('submit', function(event) {
     const formData = new FormData(event.target);
 
     const userData = {};
-    for (let [key, value] of formData) {
-        userData[key] = value;
-    }
+
+    formData.forEach((value, key) => {
+        if (userData[key]) {
+            if (Array.isArray(userData[key])) {
+                userData[key].push(value);
+            } else {
+                userData[key] = [userData[key], value];
+            }
+        } else {
+            userData[key] = value;
+        }
+    });
     
     // Виводимо дані в консоль
     console.log(userData);
@@ -33,8 +42,20 @@ function displayData(data) {
     const gender = data.gender;
     const city = data.city;
     const address = data.address;
-    const languages = data.language;  // тут можуть бути деякі відмінності, залежно від того, як ви зберігаєте мови
+    const languages = Array.isArray(data.language) ? data.language.join(", ") : data.language;
 
-    // Інший код вашої функції ...
+    const tableHTML = `
+        <table class="table table-bordered">
+            <tr><td>Ім'я</td><td>${name}</td></tr>
+            <tr><td>Прізвище</td><td>${surname}</td></tr>
+            <tr><td>Дата народження</td><td>${dob}</td></tr>
+            <tr><td>Стать</td><td>${gender}</td></tr>
+            <tr><td>Місто</td><td>${city}</td></tr>
+            <tr><td>Адреса</td><td>${address}</td></tr>
+            <tr><td>Мови</td><td>${languages}</td></tr>
+        </table>    `;
+
+    document.getElementById('outputTable').innerHTML = tableHTML;
+    document.getElementById('registrationForm').style.display = 'none';
 }
 
